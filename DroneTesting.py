aba23cdadd4ee9f60836369ac93a582054d7b57c
Erick Sandhu <141291020+essandhu@users.noreply.github.com>
2023-11-14 21:01:00 -0600

    Update DroneTesting.py

--- DroneTesting.py
+++ DroneTesting.py
@@ -1,4 +1,6 @@
-from dronekit import connect, VehicleMode, LocationGlobalRelative
+import cv2
+import numpy as np
+from dronekit import connect, VehicleMode, LocationGlobal, LocationGlobalRelative
 import dronekit_sitl
 import time
 
@@ -31,6 +33,7 @@ def armDrone():
         time.sleep(1)
     print("Vehicle armed and in GUIDED mode!")
 
+# Used to take off the drone to a specific altitude
 def takeoffDrone(targetAltitude):
     print("Taking off!")
     vehicle.simple_takeoff(targetAltitude) # Take off to target altitude
@@ -45,9 +48,70 @@ def takeoffDrone(targetAltitude):
             break
         time.sleep(1)
 
+# Function to move the drone to a specific location (global relative)
+def goto_location(lat, lon, alt):
+    target_location = LocationGlobalRelative(lat, lon, alt)
+    vehicle.simple_goto(target_location)
+    while True:
+        current_location = vehicle.location.global_relative_frame
+        distance = current_location.distance_to(target_location)
+        if distance < 1:  # Adjust this threshold as needed
+            break
+
+# Placeholder for ArUco marker detection
+def detect_aruco_markers(image):
+    # Define the ArUco dictionary and parameters
+    aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
+    parameters = cv2.aruco.DetectorParameters_create()
+
+    # Detect ArUco markers in the image
+    corners, ids, _ = cv2.aruco.detectMarkers(image, aruco_dict, parameters=parameters)
+    return corners, ids
+
+# Used to begin searching process for challenge 1
 def challenge1Search():
     print("Starting Challenge 1 Search")
-    # Search for the target
+
+    # Define field dimensions (in yards)
+    field_width = 50
+    field_height = 50
+
+    # Define grid parameters
+    grid_size = 5  # Divide the field into a 5x5 grid
+    grid_step_x = field_width / grid_size
+    grid_step_y = field_height / grid_size
+
+    # Initialize drone's position at the bottom-left corner
+    drone_x = 0
+    drone_y = 0
+
+    while True:
+        # Move the drone to the current grid cell
+        goto_location(drone_x, drone_y, 10)  # Assuming altitude of 10 meters
+
+        # Capture an image from the drone's camera
+        # needs to be added
+
+        # Detect ArUco markers in the captured image
+        corners, ids = detect_aruco_markers(frame)
+
+        # Store detected ArUco markers and their positions
+        if ids is not None:
+            for i in range(len(ids)):
+                marker_id = ids[i][0]
+                marker_corners = corners[i][0]
+                # Store marker_id and marker_corners in your data structure
+
+        # Move the drone approximately 15 yards to the right
+        drone_x += 15  # Adjust this distance as needed
+        if drone_x >= field_width:
+            # If at the right edge of the field, move forward and reset drone_x
+            drone_x = 0
+            drone_y += grid_step_y
+            if drone_y >= field_height:
+                break  # Break the loop if the end of the field is reached
+
+
 
 '''MAIN'''
 # Connect to the vehicle (in this case SITL simulator)
@@ -83,10 +147,11 @@ armDrone()
 # Take off to 10m
 takeoffDrone(10)
 
-# Simple goto 
-vehicle.simple_goto(LocationGlobalRelative(-35.363261, 149.165230, 20))
+# Begin challenge 1 search
+challenge1Search()
 
 # Close vehicle object when finished running script
+vehicle.mode = VehicleMode("LAND")
 vehicle.close()
 
 # Close the simulator
